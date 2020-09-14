e182a70852eb7297f00dafed5347a581a8f2b459
sjaugmented <sjaugmented@gmail.com>
2020-09-14 16:18:25 -0700

    map is rendering with current location

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import AppTextInput from "../components/AppTextInput";
 
 function MapScreen(props) {
-  return <MapView></MapView>;
+  const [location, setLocation] = useState();
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestPermissionsAsync();
+    if (!granted) {
+      // error - we need your location dummy
+    } else {
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      setLocation({ latitude, longitude });
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <>
+      {location && (
+        <MapView
+          // props
+          style={styles.mapStyle}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        >
+          <AppTextInput />
+        </MapView>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   mapStyle: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    flex: 1,
+    // width: Dimensions.get("window").width,
+    // height: Dimensions.get("window").height,
   },
 });
 

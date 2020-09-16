48e6bf81a77633cbf16d0565f6008e7c12bed135
sjaugmented <sjaugmented@gmail.com>
2020-09-16 11:29:51 -0700

    trimmed some fat

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StatusBar } from "expo-status-bar";
 
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
 export default function App() {
   return (

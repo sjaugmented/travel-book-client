f54bf06ab5c4d73d45f23a3345df5e29c1e926d9
sjaugmented <sjaugmented@gmail.com>
2020-09-15 22:26:03 -0700

    added context to MemoryNavigator

--- app/components/MemoryNavigator.js
+++ app/components/MemoryNavigator.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import TypeOfPlace from "./memoryCards/TypeOfPlace";
 import Transpo from "./memoryCards/Transpo";
 import PhotoSocial from "./memoryCards/PhotoSocial";
 import Done from "./memoryCards/Done";
+import NewTrip from "./NewTrip";
+import ActiveTripContext from "../context/ActiveTripContext";
 
 const Stack = createStackNavigator();
 
 function MemoryNavigator({ setMemoryVisible }) {
+  const [tripActive, setTripActive] = useContext(ActiveTripContext);
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Type" component={TypeOfPlace} />
+      {!tripActive ? (
+        <Stack.Screen name="NewTrip" component={NewTrip} />
+      ) : (
+        <Stack.Screen name="Type" component={TypeOfPlace} />
+      )}
       <Stack.Screen name="Transpo" component={Transpo} />
       <Stack.Screen name="PhotoSocial" component={PhotoSocial} />
       <Stack.Screen name="Done" component={Done} />

04f328eab4e7d8a48cd063fdc7a19c6d150e0afb
sjaugmented <sjaugmented@gmail.com>
2020-09-16 23:07:54 -0700

    working on storing user data

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -4,48 +4,10 @@ import * as Linking from "expo-linking";
 import AppButton from "../components/AppButton";
 import colors from "../config/colors";
 
-import * as AuthSession from "expo-auth-session";
 import * as Google from "expo-google-app-auth";
-
-const auth0Domain = "https://dev-r94p-cf7.us.auth0.com";
-const auth0ClientId = "y7CBRXvOHQoNJqPQqcXcZ9znVN3hBYiz";
+import UserModel from "../api/user";
 
 function WelcomeScreen({ navigation }) {
-  loginWithAuth0 = async () => {
-    let str = "";
-    const params = {
-      client_id: auth0ClientId,
-      response_type: "token",
-      scope: "openid, profile, email",
-      redirect_uri: redirectUrl,
-    };
-
-    for (const key in params) {
-      if (str != "") {
-        str += "&";
-      }
-      str += key + "=" + params[key];
-    }
-
-    console.log(`${auth0Domain}/authorize/${str}`);
-
-    const redirectUrl = await AuthSession.getRedirectUrl();
-    let authUrl = `${auth0Domain}/authorize/${str}`;
-    console.log(`Redirect URL (add this to Auth0): ${redirectUrl}`);
-    console.log(`AuthURL is:  ${authUrl}`);
-    const result = await AuthSession.startAsync({
-      authUrl: authUrl,
-    });
-
-    if (result.type === "success") {
-      console.log(result);
-      let token = result.params.access_token;
-      console.log(token);
-      this.props.setToken(token);
-      this.props.navigation.navigate("Next Screen");
-    }
-  };
-
   async function signInWithGoogleAsync() {
     try {
       const result = await Google.logInAsync({
@@ -58,6 +20,8 @@ function WelcomeScreen({ navigation }) {
 
       if (result.type === "success") {
         console.log("result", result);
+        const allUsers = await UserModel.all();
+        console.log("allUsers", allUsers);
         return result.accessToken;
       } else {
         return { cancelled: true };

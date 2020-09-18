13c84d1397c2d798ea16c896f449c3cc6a70c63d
Evan Doherty <edoherty77@gmail.com>
2020-09-18 19:14:28 -0400

    fixed one more conflict

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,82 +1,70 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { View, StyleSheet, Button, Modal } from 'react-native'
 import NativeModal from 'react-native-modal'
-import MapInput from '../components/MapInput'
 import MapView from 'react-native-maps'
 import * as Location from 'expo-location'
-=======
-import React, { useContext, useEffect, useState } from "react";
-import { View, StyleSheet, Button, Modal } from "react-native";
-import NativeModal from "react-native-modal";
-import MapView from "react-native-maps";
-import * as Location from "expo-location";
-import AsyncStorage from "@react-native-community/async-storage";
->>>>>>> submaster
+import AsyncStorage from '@react-native-community/async-storage'
 
 //Styles
-import colors from "../config/colors";
-import ButtonIcon from "../components/ButtonIcon";
+import colors from '../config/colors'
+import ButtonIcon from '../components/ButtonIcon'
 
 //Navigators
-import MenuNavigator from "../navigation/MenuNavigator";
-import MemoryNavigator from "../navigation/MemoryNavigator";
+import MenuNavigator from '../navigation/MenuNavigator'
+import MemoryNavigator from '../navigation/MemoryNavigator'
 
 //useContexts
-import MemoryContext from "../context/memoryContext";
-import TripContext from "../context/TripContext";
-import ActiveTripContext from "../context/activeTripContext";
+import MemoryContext from '../context/memoryContext'
+import TripContext from '../context/TripContext'
+import ActiveTripContext from '../context/activeTripContext'
 
 //API
-import MemoryModal from "../api/memories";
-import ModalContext from "../context/modalContext";
-import UserContext from "../context/userContext";
+import MemoryModal from '../api/memories'
+import ModalContext from '../context/modalContext'
+import UserContext from '../context/userContext'
 
-<<<<<<< HEAD
-function MapScreen({ navigation, props }) {
-=======
+import MapInput from '../components/MapInput'
 function MapScreen({ navigation }) {
-  const { user, logout } = useContext(UserContext);
->>>>>>> submaster
+  const { user, logout } = useContext(UserContext)
   //Hide and show
-  const [menuVisible, setMenuVisible] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false)
 
-  const [tripActive, setTripActive] = useState();
+  const [tripActive, setTripActive] = useState()
 
   //Memory and Trip Hooks
-  const [memory, setMemory] = useState(null);
-  const [tripName, setTripName] = useState("");
-  const [location, setLocation] = useState();
-  const [checkInPlace, setCheckInPlace] = useState("");
-  const [checkInType, setCheckInType] = useState("");
-  const [checkInTranspo, setCheckInTranspo] = useState("");
-  const [checkInPhoto, setCheckInPhoto] = useState("");
+  const [memory, setMemory] = useState(null)
+  const [tripName, setTripName] = useState('')
+  const [location, setLocation] = useState()
+  const [checkInPlace, setCheckInPlace] = useState('')
+  const [checkInType, setCheckInType] = useState('')
+  const [checkInTranspo, setCheckInTranspo] = useState('')
+  const [checkInPhoto, setCheckInPhoto] = useState('')
 
   //Hook for show trip window
-  const [pickedTrip, setPickedTrip] = useState("");
+  const [pickedTrip, setPickedTrip] = useState('')
 
   const getTripActive = async () => {
     try {
-      const tripState = await AsyncStorage.getItem("tripActive");
-      console.log("MapScreenTripState:", tripState);
-      if (tripState === "true") setTripActive(true);
-      else setTripActive(false);
+      const tripState = await AsyncStorage.getItem('tripActive')
+      console.log('MapScreenTripState:', tripState)
+      if (tripState === 'true') setTripActive(true)
+      else setTripActive(false)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const storeTripActive = async (bool) => {
     try {
-      const str = bool.toString();
-      console.log("str:", str);
-      setTripActive(bool);
-      await AsyncStorage.setItem("tripActive", str);
+      const str = bool.toString()
+      console.log('str:', str)
+      setTripActive(bool)
+      await AsyncStorage.setItem('tripActive', str)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   //setMemory with this data and call saveMemory function to add to db
   const addMemory = () => {
@@ -86,40 +74,40 @@ function MapScreen({ navigation }) {
       type: checkInType,
       transpo: checkInTranspo,
       photo: checkInPhoto,
-    };
-    setMemory(memoryData);
-    if (!tripActive) storeTripActive(true);
-    saveMemory(memoryData);
-    setModalVisible(false);
-  };
+    }
+    setMemory(memoryData)
+    if (!tripActive) storeTripActive(true)
+    saveMemory(memoryData)
+    setModalVisible(false)
+  }
 
   //Adding memory to db
   const saveMemory = async (memory) => {
     const data = {
       memory,
       tripName,
-    };
-    const result = await MemoryModal.create(data);
-    setMemory(null);
-  };
+    }
+    const result = await MemoryModal.create(data)
+    setMemory(null)
+  }
 
   //Setting latitude and longitude for current location
   const getLocation = async () => {
-    const { granted } = await Location.requestPermissionsAsync();
+    const { granted } = await Location.requestPermissionsAsync()
     if (!granted) {
       // error - we need your location dummy
     } else {
       const {
         coords: { latitude, longitude },
-      } = await Location.getCurrentPositionAsync();
-      setLocation({ latitude, longitude });
+      } = await Location.getCurrentPositionAsync()
+      setLocation({ latitude, longitude })
     }
-  };
+  }
 
   useEffect(() => {
-    getTripActive();
-    getLocation();
-  }, []);
+    getTripActive()
+    getLocation()
+  }, [])
 
   return (
     <>
@@ -163,7 +151,7 @@ function MapScreen({ navigation }) {
           )}
           <ButtonIcon
             style={styles.menuButton}
-            name={"xbox-controller-menu"}
+            name={'xbox-controller-menu'}
             size={65}
             backgroundColor={colors.light}
             iconColor={colors.secondary}
@@ -185,8 +173,8 @@ function MapScreen({ navigation }) {
       >
         <View style={styles.menuView}>
           <ButtonIcon
-            style={{ alignSelf: "center" }}
-            name={"chevron-down"}
+            style={{ alignSelf: 'center' }}
+            name={'chevron-down'}
             backgroundColor={colors.light}
             iconColor={colors.primary}
             onPress={() => setMenuVisible(false)}
@@ -244,26 +232,26 @@ function MapScreen({ navigation }) {
         </View>
       </NativeModal>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   addButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 200,
   },
   menuButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 75,
   },
   confirmation: {
     fontSize: 30,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
   },
   mapStyle: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
   memoryView: {
     flex: 1,
@@ -272,7 +260,7 @@ const styles = StyleSheet.create({
     // margin: -20,
     backgroundColor: colors.light,
     padding: 35,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -289,8 +277,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
     borderRadius: 20,
     // padding: 35,
-    height: "80%",
-    shadowColor: "#000",
+    height: '80%',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -299,6 +287,6 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 5,
   },
-});
+})
 
-export default MapScreen;
+export default MapScreen

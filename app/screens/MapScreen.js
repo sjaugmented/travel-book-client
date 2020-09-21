6fbae782e2c846312fd997c5b61f04ebf513e79b
sjaugmented <sjaugmented@gmail.com>
2020-09-20 22:23:35 -0700

    added button shadows to map screen

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,31 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { View, StyleSheet, Button, Modal, Dimensions } from 'react-native'
-import NativeModal from 'react-native-modal'
-import MapView, { Marker } from 'react-native-maps'
-import * as Location from 'expo-location'
-import AsyncStorage from '@react-native-community/async-storage'
-import { getDistance } from 'geolib'
+import React, { useContext, useEffect, useState } from "react"
+import { View, StyleSheet, Button, Modal, Dimensions } from "react-native"
+import NativeModal from "react-native-modal"
+import MapView, { Marker } from "react-native-maps"
+import * as Location from "expo-location"
+import AsyncStorage from "@react-native-community/async-storage"
+import { getDistance } from "geolib"
 
 //Styles
-import colors from '../config/colors'
-import ButtonIcon from '../components/ButtonIcon'
+import colors from "../config/colors"
+import ButtonIcon from "../components/ButtonIcon"
 
 //Navigators
-import MenuNavigator from '../navigation/MenuNavigator'
-import MemoryNavigator from '../navigation/MemoryNavigator'
+import MenuNavigator from "../navigation/MenuNavigator"
+import MemoryNavigator from "../navigation/MemoryNavigator"
 
 //useContexts
-import MemoryContext from '../context/memoryContext'
-import TripContext from '../context/TripContext'
-import TripShowContext from '../context/TripShowContext'
-import ActiveTripContext from '../context/activeTripContext'
+import MemoryContext from "../context/memoryContext"
+import TripContext from "../context/TripContext"
+import TripShowContext from "../context/TripShowContext"
+import ActiveTripContext from "../context/activeTripContext"
 
 //API
-import MemoryModel from '../api/memories'
-import ModalContext from '../context/modalContext'
-import UserContext from '../context/userContext'
+import MemoryModel from "../api/memories"
+import ModalContext from "../context/modalContext"
+import UserContext from "../context/userContext"
 
-import MapInput from '../components/MapInput'
+import MapInput from "../components/MapInput"
 
 function MapScreen({ navigation }) {
   //useContext
@@ -35,27 +35,27 @@ function MapScreen({ navigation }) {
   const [menuVisible, setMenuVisible] = useState(false)
   const [modalVisible, setModalVisible] = useState(false)
 
-  const [tripActive, setTripActive] = useState('')
+  const [tripActive, setTripActive] = useState("")
 
   //Memory and Trip Hooks
-  const [allMemories, setAllMemories] = useState('')
+  const [allMemories, setAllMemories] = useState("")
   const [memory, setMemory] = useState(null)
-  const [tripName, setTripName] = useState('')
+  const [tripName, setTripName] = useState("")
   const [location, setLocation] = useState()
-  const [checkInPlace, setCheckInPlace] = useState('')
-  const [checkInType, setCheckInType] = useState('')
-  const [checkInTranspo, setCheckInTranspo] = useState('')
-  const [checkInPhoto, setCheckInPhoto] = useState('')
-  const [memoryLocation, setMemoryLocation] = useState('')
-  const [prevLocation, setPrevLocation] = useState('')
+  const [checkInPlace, setCheckInPlace] = useState("")
+  const [checkInType, setCheckInType] = useState("")
+  const [checkInTranspo, setCheckInTranspo] = useState("")
+  const [checkInPhoto, setCheckInPhoto] = useState("")
+  const [memoryLocation, setMemoryLocation] = useState("")
+  const [prevLocation, setPrevLocation] = useState("")
 
   //Hook for show trip window
-  const [pickedTrip, setPickedTrip] = useState('')
+  const [pickedTrip, setPickedTrip] = useState("")
 
   const getTripActive = async () => {
     try {
-      const tripState = await AsyncStorage.getItem('tripActive')
-      if (tripState === 'true') setTripActive(true)
+      const tripState = await AsyncStorage.getItem("tripActive")
+      if (tripState === "true") setTripActive(true)
       else setTripActive(false)
     } catch (error) {
       console.log(error)
@@ -66,7 +66,7 @@ function MapScreen({ navigation }) {
     try {
       const str = bool.toString()
       setTripActive(bool)
-      await AsyncStorage.setItem('tripActive', str)
+      await AsyncStorage.setItem("tripActive", str)
     } catch (error) {
       console.log(error)
     }
@@ -85,10 +85,10 @@ function MapScreen({ navigation }) {
     if (!tripActive) storeTripActive(true)
     saveMemory(memoryData)
     setModalVisible(false)
-    setCheckInType('')
-    setCheckInPlace('')
-    setCheckInTranspo('')
-    setCheckInPhoto('')
+    setCheckInType("")
+    setCheckInPlace("")
+    setCheckInTranspo("")
+    setCheckInPhoto("")
   }
 
   const refreshMap = (delay) => {
@@ -163,15 +163,8 @@ function MapScreen({ navigation }) {
           showsUserLocation={true}
         >
           {allMemories
-            ? allMemories.map((marker, index) => (
-                <Marker
-                  pinColor="blue"
-                  key={index}
-                  title={marker.locationName}
-                  coordinate={marker.location}
-                />
-              ))
-            : console.log('none')}
+            ? allMemories.map((marker, index) => <Marker pinColor="blue" key={index} title={marker.locationName} coordinate={marker.location} />)
+            : console.log("none")}
         </MapView>
       )}
       {!tripActive ? (
@@ -197,7 +190,7 @@ function MapScreen({ navigation }) {
       )}
       <ButtonIcon
         style={styles.menuButton}
-        name={'xbox-controller-menu'}
+        name={"xbox-controller-menu"}
         size={65}
         backgroundColor="transparent"
         iconColor={colors.secondary}
@@ -237,10 +230,7 @@ function MapScreen({ navigation }) {
               }}
             >
               <ModalContext.Provider value={{ setMenuVisible }}>
-                <MenuNavigator
-                  tripActive={tripActive}
-                  storeTripActive={storeTripActive}
-                />
+                <MenuNavigator tripActive={tripActive} storeTripActive={storeTripActive} />
               </ModalContext.Provider>
             </ActiveTripContext.Provider>
           </TripContext.Provider>
@@ -260,9 +250,7 @@ function MapScreen({ navigation }) {
         style={styles.memModal}
       >
         <View style={styles.memoryView}>
-          <ActiveTripContext.Provider
-            value={{ tripActive: tripActive, storeTripActive: storeTripActive }}
-          >
+          <ActiveTripContext.Provider value={{ tripActive: tripActive, storeTripActive: storeTripActive }}>
             <MemoryContext.Provider
               style={styles.activeTrip}
               value={{
@@ -293,26 +281,42 @@ function MapScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   addButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 175,
-    left: '50%',
+    left: "49%",
     marginLeft: -50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 10,
+    elevation: 5,
   },
   menuButton: {
-    position: 'absolute',
+    position: "absolute",
 
     bottom: 75,
-    left: '50%',
+    left: "49%",
     marginLeft: -32.5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 10,
+    elevation: 5,
   },
   confirmation: {
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   mapStyle: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   memModal: {
     marginHorizontal: 50,
@@ -328,7 +332,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 0,
     borderRadius: 50,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -343,8 +347,8 @@ const styles = StyleSheet.create({
     marginLeft: 0,
     marginTop: 250,
     borderRadius: 20,
-    height: '80%',
-    shadowColor: '#000',
+    height: "80%",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
